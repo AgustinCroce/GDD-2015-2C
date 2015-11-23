88d39389b80d61e9360d3277ebe6538781ac313c
FedeG <federicog@exo.com.ar>
2015-11-23 09:31:13 -0300

    Agrego formularios de agregar ruta  #6

---
+++ AerolineaFrba/Abm Ruta/AltaRuta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AerolineaFrba.Abm_Ruta
+{
+
+
+    public partial class AltaRuta : AerolineaFrba.Abm_Ruta.BaseRuta
+    {
+        public AltaRuta()
+        {
+            InitializeComponent();
+            this.enabledButtons.RegisterTextBox(TB_codigo_unico);
+            this.enabledButtons.RegisterTextBox(TB_precio_kg);
+            this.enabledButtons.RegisterTextBox(TB_precio_pasaje);
+            this.enabledButtons.RegisterButton(BT_guardar);
+        }
+
+        private void BT_guardar_Click(object sender, EventArgs e)
+        {
+            SqlCommand spALtaRuta = this.db.GetStoreProcedure("TS.spAltaRuta");
+            spALtaRuta.Parameters.Add(new SqlParameter("@origen", CB_origen.SelectedValue));
+            spALtaRuta.Parameters.Add(new SqlParameter("@destino", CB_destino.SelectedValue));
+            spALtaRuta.Parameters.Add(new SqlParameter("@servicio", CB_servicio.SelectedValue));
+            spALtaRuta.Parameters.Add(new SqlParameter("@codigo", Convert.ToInt64(TB_codigo_unico.Text)));
+            spALtaRuta.Parameters.Add(new SqlParameter("@precio_kg", Convert.ToInt64(TB_precio_kg.Text)));
+            spALtaRuta.Parameters.Add(new SqlParameter("@precio_pasaje", Convert.ToInt64(TB_precio_pasaje.Text)));
+            spALtaRuta.ExecuteNonQuery();
+        }
+    }
+}
