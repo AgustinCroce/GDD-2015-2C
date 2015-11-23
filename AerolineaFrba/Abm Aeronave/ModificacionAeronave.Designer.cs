c44b025eddc7baa281abaab73d00db74b300392d
FedeG <federicog@exo.com.ar>
2015-11-23 09:13:15 -0300

    Agrego modificacion de aeronave  #7

---
+++ AerolineaFrba/Abm Aeronave/ModificacionAeronave.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace AerolineaFrba.Abm_Aeronave
+{
+    partial class ModificacionAeronave
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BT_guardar
+            // 
+            this.BT_guardar.Click += new System.EventHandler(this.BT_guardar_Click);
+            // 
+            // ModificacionAeronave
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 327);
+            this.Name = "ModificacionAeronave";
+            this.Text = "ModificacionAeronave";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ AerolineaFrba/Abm Aeronave/ModificacionAeronave.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AerolineaFrba.Abm_Aeronave
+{
+    public partial class ModificacionAeronave : AerolineaFrba.Abm_Aeronave.BaseAeronave
+    {
+        public ModificacionAeronave(DataGridViewRow selected)
+        {
+            InitializeComponent();
+            TB_fabricante.Text = selected.Cells["Fabricante"].Value.ToString();
+            TB_kg_disponibles.Text = selected.Cells["KG Disponible"].Value.ToString();
+            TB_matricula.Text = selected.Cells["Matricula"].Value.ToString();
+            TB_modelo.Text = selected.Cells["Modelo"].Value.ToString();
+            TB_numero.Text = selected.Cells["Numero"].Value.ToString();
+        }
+
+        private void BT_guardar_Click(object sender, EventArgs e)
+        {
+            SqlCommand spModificarAeronave = this.db.GetStoreProcedure("TS.spModificarAeronave");
+            spModificarAeronave.Parameters.Add(new SqlParameter("@modelo", TB_modelo.Text));
+            spModificarAeronave.Parameters.Add(new SqlParameter("@matricula", TB_matricula));
+            spModificarAeronave.Parameters.Add(new SqlParameter("@fabricante", TB_fabricante));
+            spModificarAeronave.Parameters.Add(new SqlParameter("@numero", Convert.ToInt64(TB_numero.Text)));
+            spModificarAeronave.Parameters.Add(new SqlParameter("@butacas_v", Convert.ToInt64(TB_butacas_ventanilla.Text)));
+            spModificarAeronave.Parameters.Add(new SqlParameter("@butacas_p", Convert.ToInt64(TB_butacas_pasillo.Text)));
+            spModificarAeronave.Parameters.Add(new SqlParameter("@kg_disponibles", Convert.ToInt64(TB_kg_disponibles.Text)));
+            spModificarAeronave.ExecuteNonQuery();
+        }
+    }
+}
