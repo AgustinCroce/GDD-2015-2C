1b994b2577c1add65c809404bbfd8cc15876885f
FedeG <federicog@exo.com.ar>
2015-12-01 06:05:15 -0300

    Modificacion de Rol

---
+++ AerolineaFrba/Abm Rol/ModificacionRol.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace AerolineaFrba.Abm_Rol
+{
+    partial class ModificacionRol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BT_guardar
+            // 
+            this.BT_guardar.Click += new System.EventHandler(this.BT_guardar_Click);
+            // 
+            // TB_nombre
+            // 
+            this.TB_nombre.Enabled = false;
+            // 
+            // ModificacionRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 134);
+            this.Name = "ModificacionRol";
+            this.Text = "ModificacionRol";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
