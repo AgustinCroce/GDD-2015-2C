220974888a80773743ba0423a8ab7f8feb3f6e99
FedeG <federicog@exo.com.ar>
2015-12-01 06:04:29 -0300

    Modificacion de Ciudad

---
+++ AerolineaFrba/Abm Ciudad/ModificacionCiudad.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace AerolineaFrba.Abm_Ciudad
+{
+    partial class ModificacionCiudad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BT_guardar
+            // 
+            this.BT_guardar.Click += new System.EventHandler(this.BT_guardar_Click);
+            // 
+            // ModificacionCiudad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 132);
+            this.Name = "ModificacionCiudad";
+            this.Text = "ModificacionCiudad";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ AerolineaFrba/Abm Ciudad/ModificacionCiudad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AerolineaFrba.Abm_Ciudad
+{
+    public partial class ModificacionCiudad : AerolineaFrba.Abm_Ciudad.BaseCiudad
+    {
+        public ModificacionCiudad(DataGridViewRow selected)
+        {
+            InitializeComponent();
+            TB_nombre.Text = selected.Cells["Nombre"].Value.ToString();
+            TB_codigo.Text = selected.Cells["Codigo"].Value.ToString();
+        }
+
+        private void BT_guardar_Click(object sender, EventArgs e)
+        {
+            SqlCommand spModificarCiudad = this.db.GetStoreProcedure("TS.spModificarCiudad");
+            spModificarCiudad.Parameters.Add(new SqlParameter("@nombre", TB_nombre.Text));
+            spModificarCiudad.Parameters.Add(new SqlParameter("@codigo", Convert.ToInt64(TB_codigo.Text)));
+            spModificarCiudad.ExecuteNonQuery();
+            this.Close();
+        }
+    }
+}
