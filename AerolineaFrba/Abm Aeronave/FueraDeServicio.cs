a8d83584e0f3a0515c8bbefc4258dd9a3e2a42df
FedeG <federicog@exo.com.ar>
2015-12-05 17:50:49 -0300

    Form para agregar fechas de fuera de serivcio en aeronave

---
+++ AerolineaFrba/Abm Aeronave/FueraDeServicio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AerolineaFrba.Abm_Aeronave
+{
+    public partial class FueraDeServicio : Form
+    {
+        public DateTime desde, hasta;
+
+        public FueraDeServicio()
+        {
+            InitializeComponent();
+            DTP_desde.MinDate = Convert.ToDateTime(AerolineaFrba.Properties.Settings.Default.FechaSistema);
+            DTP_hasta.MinDate = Convert.ToDateTime(AerolineaFrba.Properties.Settings.Default.FechaSistema);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BT_Aceptar_Click(object sender, EventArgs e)
+        {
+            this.desde = DTP_desde.Value;
+            this.hasta = DTP_hasta.Value;
+            this.Close();
+        }
+    }
+}
