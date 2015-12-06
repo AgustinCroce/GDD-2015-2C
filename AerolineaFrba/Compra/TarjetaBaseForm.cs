8d944d3cf19448616f525c46cbe7e0747afc479d
FedeG <federicog@exo.com.ar>
2015-12-05 21:49:18 -0300

    Validator de campos para tarjetas

--- AerolineaFrba/Compra/TarjetaBaseForm.cs
+++ AerolineaFrba/Compra/TarjetaBaseForm.cs
@@ -13,9 +13,14 @@ namespace AerolineaFrba.Compra
 {
     public partial class TarjetaBaseForm : Form
     {
+        public Commons.Validator validator;
         public TarjetaBaseForm()
         {
             InitializeComponent();
+            this.validator = new Commons.Validator();
+            cardDateTextBox.KeyPress += InputNumField_KeyPress;
+            cardCodeTextBox.KeyPress += InputNumField_KeyPress;
+            cardNumberTextBoxt.KeyPress += InputNumField_KeyPress;
             string queryCiudades = "SELECT TipoTar_Nombre, TipoTar_Cod FROM TS.Tipo_Tarjeta";
             DbComunicator db = new DbComunicator();
             cardEmitterComboBox.DataSource = new BindingSource(db.GetQueryDictionary(queryCiudades, "TipoTar_Nombre", "TipoTar_Cod"), null);
@@ -24,5 +29,9 @@ namespace AerolineaFrba.Compra
             db.CerrarConexion();
         }
 
+        private void InputNumField_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            this.validator.KeyPressBinding(this.validator.validateInt, false, e);
+        }
     }
 }
