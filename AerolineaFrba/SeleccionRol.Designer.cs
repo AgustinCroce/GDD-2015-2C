1657b375e9477e29ec4ecd806018875823439a36
FedeG <federicog@exo.com.ar>
2015-12-08 11:43:44 -0300

    Agrego seleccion de rol y seleccion de funcionalidad para el login

---
+++ AerolineaFrba/SeleccionRol.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace AerolineaFrba
+{
+    partial class SeleccionRol
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CB_roles = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Aceptar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Cancelar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Rol";
+            // 
+            // CB_roles
+            // 
+            this.CB_roles.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_roles.FormattingEnabled = true;
+            this.CB_roles.Location = new System.Drawing.Point(65, 12);
+            this.CB_roles.Name = "CB_roles";
+            this.CB_roles.Size = new System.Drawing.Size(181, 21);
+            this.CB_roles.TabIndex = 4;
+            // 
+            // SeleccionRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(256, 88);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CB_roles);
+            this.Name = "SeleccionRol";
+            this.Text = "Seleccion de Rol";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CB_roles;
+    }
+}
---
+++ AerolineaFrba/SeleccionRol.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AerolineaFrba.Commons;
+
+namespace AerolineaFrba
+{
+    public partial class SeleccionRol : Form
+    {
+        string rol, username;
+        DbComunicator db;
+
+        public SeleccionRol(string user)
+        {
+            InitializeComponent();
+            this.username = user;
+            this.db = new DbComunicator();
+            string queryRol = "SELECT RU.Rol_Nombre Nombre FROM TS.Rol_Usuario AS RU WHERE RU.Usr_Username='" + username + "' AND RU.Rol_Usuario_Borrado=0";
+            Dictionary<object, object> Roles = this.db.GetQueryDictionary(queryRol, "Nombre", "Nombre");
+            CB_roles.DataSource = new BindingSource(Roles, null);
+            CB_roles.DisplayMember = "Value";
+            CB_roles.ValueMember = "Key";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rol = CB_roles.SelectedValue.ToString();
+            SeleccionFuncionalidad re = new SeleccionFuncionalidad(rol, username);
+            re.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
